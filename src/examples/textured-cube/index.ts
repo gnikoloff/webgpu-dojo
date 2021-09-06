01f27b7198db0938d05c9efe0ab36dfb40e26672
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2021-09-06 10:25:36 +0200

    feat(textured-cube): bootstrap demo

---
+++ src/examples/textured-cube/index.ts
@@ -0,0 +1,240 @@
+import {
+  PerspectiveCamera,
+  GeometryUtils,
+  CameraController,
+} from '../../lib/hwoa-rang-gl'
+
+import '../index.css'
+
+const VERTEX_SHADER = `
+  [[block]]
+  struct Uniforms {
+    projectionMatrix : mat4x4<f32>;
+    viewMatrix : mat4x4<f32>;
+  };
+
+  [[group(0), binding(0)]]
+  var<uniform> uniforms: Uniforms;
+
+  struct Input {
+    [[location(0)]] position: vec4<f32>;
+    [[location(1)]] uv: vec2<f32>;
+  };
+
+  struct Output {
+    [[builtin(position)]] Position: vec4<f32>;
+    [[location(0)]] uv: vec2<f32>;
+  };
+
+  [[stage(vertex)]]
+  fn main (input: Input) -> Output {
+    var output: Output;
+
+    output.Position = uniforms.projectionMatrix * uniforms.viewMatrix * input.position;
+    output.uv = input.uv;
+
+    return output;
+  }
+`
+
+const FRAGMENT_SHADER = `
+  struct Input {
+    [[location(0)]] uv: vec2<f32>;
+  };
+
+  [[stage(fragment)]]
+
+  fn main (input: Input) -> [[location(0)]] vec4<f32> {
+    return vec4<f32>(input.uv, 0.0, 1.0);
+  }
+`
+
+;(async () => {
+  const canvas = document.getElementById('gpu-c') as HTMLCanvasElement
+  canvas.width = innerWidth * devicePixelRatio
+  canvas.height = innerHeight * devicePixelRatio
+  canvas.style.setProperty('width', `${innerWidth}px`)
+  canvas.style.setProperty('height', `${innerHeight}px`)
+
+  const adapter = await navigator.gpu?.requestAdapter()
+  const device = await adapter?.requestDevice()
+  const context = canvas.getContext('webgpu')
+
+  const presentationFormat = context.getPreferredFormat(adapter)
+
+  const primitiveType = 'triangle-list'
+
+  context.configure({
+    device,
+    format: presentationFormat,
+  })
+
+  const { vertices, uv, indices } = GeometryUtils.createBox()
+  const vertexBuffer = device.createBuffer({
+    size: vertices.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    mappedAtCreation: true,
+  })
+  new Float32Array(vertexBuffer.getMappedRange()).set(vertices)
+  vertexBuffer.unmap()
+
+  const uvBuffer = device.createBuffer({
+    size: uv.byteLength,
+    usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
+    mappedAtCreation: true,
+  })
+  new Float32Array(uvBuffer.getMappedRange()).set(uv)
+  uvBuffer.unmap()
+
+  const indexBuffer = device.createBuffer({
+    size: indices.byteLength,
+    usage: GPUBufferUsage.INDEX | GPUBufferUsage.COPY_DST,
+    mappedAtCreation: true,
+  })
+  if (indices instanceof Uint16Array) {
+    new Uint16Array(indexBuffer.getMappedRange()).set(indices)
+  } else {
+    new Uint32Array(indexBuffer.getMappedRange()).set(indices)
+  }
+
+  indexBuffer.unmap()
+
+  const pipeline = device.createRenderPipeline({
+    vertex: {
+      module: device.createShaderModule({
+        code: VERTEX_SHADER,
+      }),
+      entryPoint: 'main',
+      buffers: [
+        {
+          arrayStride: 3 * Float32Array.BYTES_PER_ELEMENT,
+          attributes: [
+            {
+              shaderLocation: 0,
+              format: 'float32x3',
+              offset: 0,
+            },
+          ],
+        },
+        {
+          arrayStride: 2 * Float32Array.BYTES_PER_ELEMENT,
+          attributes: [
+            {
+              shaderLocation: 1,
+              format: 'float32x2',
+              offset: 0,
+            },
+          ],
+        },
+      ],
+    },
+    fragment: {
+      module: device.createShaderModule({
+        code: FRAGMENT_SHADER,
+      }),
+      entryPoint: 'main',
+      targets: [
+        {
+          format: presentationFormat,
+        },
+      ],
+    },
+    primitive: {
+      topology: primitiveType,
+      stripIndexFormat: undefined,
+      // cullMode: 'back',
+    },
+    depthStencil: {
+      format: 'depth24plus',
+      depthWriteEnabled: true,
+      depthCompare: 'less',
+    },
+  })
+
+  const vertexUniformBuffer = device.createBuffer({
+    size: (16 + 16) * Float32Array.BYTES_PER_ELEMENT,
+    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+  })
+
+  const uniformBindGroup = device.createBindGroup({
+    layout: pipeline.getBindGroupLayout(0),
+    entries: [
+      {
+        binding: 0,
+        resource: {
+          buffer: vertexUniformBuffer,
+          offset: 0,
+          size: (16 + 16) * Float32Array.BYTES_PER_ELEMENT,
+        },
+      },
+    ],
+  })
+
+  const perspCamera = new PerspectiveCamera(
+    (45 * Math.PI) / 180,
+    canvas.width / canvas.height,
+    0.1,
+    100,
+  )
+  perspCamera.setPosition({ x: 2, y: 0, z: 3 })
+  perspCamera.lookAt([0, 0, 0])
+  perspCamera.updateProjectionMatrix()
+  perspCamera.updateViewMatrix()
+
+  new CameraController(perspCamera)
+
+  const textureDepth = device.createTexture({
+    size: [canvas.width, canvas.height, 1],
+    format: 'depth24plus',
+    usage: GPUTextureUsage.RENDER_ATTACHMENT,
+  })
+
+  let textureView
+
+  requestAnimationFrame(drawFrame)
+
+  function drawFrame() {
+    requestAnimationFrame(drawFrame)
+
+    textureView = context.getCurrentTexture().createView()
+
+    const commandEncoder = device.createCommandEncoder()
+    const renderPass = commandEncoder.beginRenderPass({
+      colorAttachments: [
+        {
+          view: textureView,
+          loadValue: [0.1, 0.1, 0.1, 1.0],
+          storeOp: 'store',
+        },
+      ],
+      depthStencilAttachment: {
+        view: textureDepth.createView(),
+        depthLoadValue: 1,
+        depthStoreOp: 'store',
+        stencilLoadValue: 0,
+        stencilStoreOp: 'store',
+      },
+    })
+    device.queue.writeBuffer(
+      vertexUniformBuffer,
+      0,
+      perspCamera.projectionMatrix as ArrayBuffer,
+    )
+    device.queue.writeBuffer(
+      vertexUniformBuffer,
+      16 * Float32Array.BYTES_PER_ELEMENT,
+      perspCamera.viewMatrix as ArrayBuffer,
+    )
+    renderPass.setPipeline(pipeline)
+    renderPass.setVertexBuffer(0, vertexBuffer)
+    renderPass.setVertexBuffer(1, uvBuffer)
+    renderPass.setIndexBuffer(
+      indexBuffer,
+      indices instanceof Uint16Array ? 'uint16' : 'uint32',
+    )
+    renderPass.setBindGroup(0, uniformBindGroup)
+    renderPass.drawIndexed(indices.length)
+    renderPass.endPass()
+    device.queue.submit([commandEncoder.finish()])
+  }
+})()
